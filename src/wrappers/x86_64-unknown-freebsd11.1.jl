02f7ed74e854d76eb72eacbcd4f3974b56dc9dff
jlbuild <juliabuildbot@gmail.com>
2020-04-02 00:57:56 +0000

    Libuuid_jll build 2.34.0+4

--- src/wrappers/x86_64-unknown-freebsd11.1.jl
+++ src/wrappers/x86_64-unknown-freebsd11.1.jl
@@ -24,11 +24,13 @@ const libuuid = "libuuid.so.1"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"Libuuid")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    global libuuid_path = abspath(joinpath(artifact"Libuuid", libuuid_splitpath...))
+    # We first need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    global libuuid_path = normpath(joinpath(artifact_dir, libuuid_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
